afa162be0b2c98f4c7dd2c307d95865f557f463d
Kevin <kevs272@gmail.com>
2023-11-09 12:33:38 +0100

    remove can setup again (handled on host level now)

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -12,12 +12,8 @@ if [ ! -e "CONTAINER_INITIALIZED_PLACEHOLDER" ]; then
     echo "-- First container startup --"
     catkin build
     touch "CONTAINER_INITIALIZED_PLACEHOLDER" # <== This placeholder file used in the github action to check when colcon build is done, do not remove
-    source "/root/ros_ws/devel/setup.bash"
-    modprobe gs_usb
-    rosrun scout_bringup setup_can2usb.bash
 fi
     source "/root/ros_ws/devel/setup.bash"
-    rosrun scout_bringup bringup_can2usb.bash
     roslaunch scout_exploration navigation.launch # <== change to your launch file // comment out if you don't want auto launch
 
 exec "$@"
