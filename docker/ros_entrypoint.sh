7aba471d29a9e79c7c5ee0105c9c8c4050fdf126
Kevin <kevs272@gmail.com>
2023-11-09 11:28:21 +0100

    add can setup routine to container startup

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -12,8 +12,12 @@ if [ ! -e "CONTAINER_INITIALIZED_PLACEHOLDER" ]; then
     echo "-- First container startup --"
     catkin build
     touch "CONTAINER_INITIALIZED_PLACEHOLDER" # <== This placeholder file used in the github action to check when colcon build is done, do not remove
+    source "/root/ros_ws/devel/setup.bash"
+    modprobe gs_usb
+    rosrun scout_bringup setup_can2usb.bash
 fi
     source "/root/ros_ws/devel/setup.bash"
-    roslaunch 2d_slam mapping_pipeline.launch # <== change to your launch file // comment out if you don't want auto launch
+    rosrun scout_bringup bringup_can2usb.bash
+    roslaunch scout_exploration navigation.launch # <== change to your launch file // comment out if you don't want auto launch
 
 exec "$@"
